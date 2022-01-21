8b0f8d8633214152bbbbac2908a724607f5b8059
Justinas Rumševičius <justinasr@users.noreply.github.com>
2022-01-21 16:25:52 +0100

    Pylint warnings

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import subprocess
-import logging
 import json
-import sys
-import os.path
-import time
+import logging
 import os
+import os.path
+import subprocess
+import sys
+import time
 
 
 logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s',
                     level=logging.DEBUG if '--debug' in sys.argv else logging.INFO)
 logger = logging.getLogger()
 
-REPO_NAME = 'GridpackExtravaganza'
-
+CARDS_REPO_DIR = 'GridpackExtravaganza'
+CONDOR_STATUS = {'0': 'UNEXPLAINED',
+                 '1': 'IDLE',
+                 '2': 'RUN',
+                 '3': 'REMOVED',
+                 '4': 'DONE',
+                 '5': 'HOLD',
+                 '6': 'SUBMISSION ERROR'}
 
 def run_command(command):
     """
@@ -35,7 +41,7 @@ def get_jobs_in_condor():
     Return a dictionary where key is job id and value is status (IDLE, RUN, DONE)
     """
     cmd = 'condor_q -af:h ClusterId JobStatus'
-    stdout, stderr, code = run_command(cmd)
+    stdout, stderr, _ = run_command(cmd)
     lines = stdout.split('\n')
     if not lines or 'ClusterId JobStatus' not in lines[0]:
         logger.error('HTCondor is failing:\n%s\n%s', stdout, stderr)
@@ -45,16 +51,12 @@ def get_jobs_in_condor():
     lines = lines[1:]
     for line in lines:
         columns = line.split()
-        if not len(columns):
+        if not columns:
             continue
 
         job_id = columns[0]
-        if columns[1] == '4':
-            jobs_dict[job_id] = 'DONE'
-        elif columns[1] == '2':
-            jobs_dict[job_id] = 'RUN'
-        elif columns[1] == '1':
-            jobs_dict[job_id] = 'IDLE'
+        job_status = columns[1]
+        jobs_dict[job_id] = CONDOR_STATUS.get(job_status, 'REMOVED')
 
     logger.info('Job status in HTCondor:%s', json.dumps(jobs_dict))
     return jobs_dict
@@ -62,27 +64,37 @@ def get_jobs_in_condor():
 
 def get_new_cards():
     """
+    Get list of changed .dat files in /cards/ directories
     """
-    command = [f'cd {REPO_NAME}',
+    command = [f'cd {CARDS_REPO_DIR}',
                'old_commit=$(git rev-parse HEAD)',
                'git pull --quiet',
                'new_commit=$(git rev-parse HEAD)',
+               # Get only files that are in /cards/ directory and end with .dat
                'git diff --name-only $old_commit $new_commit | grep "/cards/.*\\.dat$"']
     stdout, stderr, code = run_command(command)
     if code != 0:
-        logger.error('No changed files: %s', stderr)
+        logger.error('No changed files or error: %s', stderr)
         return []
 
-    files = [line.strip() for line in stdout.split('\n') if line.endswith('.dat')]
-    files = [f'{REPO_NAME}/{f}' for f in files]
+    files = [line.strip() for line in stdout.split('\n') if line.strip()]
+    files = [f'{CARDS_REPO_DIR}/{f}' for f in files]
+    logger.debug('Changed files:\n  %s', '\n  '.join(files))
     return files
 
 
 def get_existing_files(filelist):
+    """
+    For a list of paths, return only those paths that point to an existing file
+    (filter-out no longer existing files)
+    """
     return [f for f in filelist if os.path.isfile(f)]
 
 
 def make_script(dataset_name):
+    """
+    Make a script that should be run in batch system
+    """
     cmd = ['#!/usr/bin/env bash',
            'export HOME=$(pwd)',
            'wget https://cms-project-generators.web.cern.ch/cms-project-generators/pdmvtest.tar.gz',
@@ -96,6 +108,9 @@ def make_script(dataset_name):
 
 
 def make_job_description(dataset_name):
+    """
+    Make an HTCondor job description file
+    """
     dsc = ['universe              = vanilla',
            f'executable           = {dataset_name}.sh',
            f'output               = {dataset_name}.out',
@@ -113,7 +128,8 @@ def make_job_description(dataset_name):
            '+AcctGroupUser        = "pdmvserv"',
            '+DESIRED_Sites        = "T2_CH_CERN"',
            '+REQUIRED_OS          = "rhel7"',
-           'leave_in_queue        = JobStatus == 4 && (CompletionDate =?= UNDEFINED || ((CurrentTime - CompletionDate) < 7200))',
+           'leave_in_queue        = JobStatus == 4 && (CompletionDate =?= UNDEFINED ||' # same line
+           '                        ((CurrentTime - CompletionDate) < 7200))',
            # Lines below are used in monitoring
            '+CMS_Type             = "test"',
            '+CMS_JobType          = "PdmVGridpack"',
@@ -124,13 +140,19 @@ def make_job_description(dataset_name):
     return '\n'.join(dsc)
 
 
-def get_files(dir):
-    return [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
+def get_files(directory):
+    """
+    Return all files in given directory
+    """
+    return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 
 
-def get_card_name(dir):
-    files = get_files(dir)
-    card_files = [f for f in files if f.endswith('_proc_card.dat') or f.endswith('_run_card.dat')]
+def get_card_name(directory):
+    """
+    Get card name of directory from _proc_card.dat or _run_card.dat file
+    """
+    files = get_files(directory)
+    card_files = [f for f in files if f.endswith(('_proc_card.dat', '_run_card.dat'))]
     if not card_files:
         return None
 
@@ -138,13 +160,16 @@ def get_card_name(dir):
 
 
 def create_jobs_for_cards(filelist):
+    """
+    Create job files and submit them for given list of changed files
+    """
     # Get list of directories
-    dirs = sorted(list(set(os.path.dirname(f) for f in filelist)))
+    directories = sorted(list(set(os.path.dirname(f) for f in filelist)))
 
-    logger.info('Directories: %s', dirs)
-    for dir in dirs:
-        logger.info('Working on %s', dir)
-        card_name = get_card_name(dir)
+    logger.info('Directories: %s', directories)
+    for directory in directories:
+        logger.info('Working on %s', directory)
+        card_name = get_card_name(directory)
         logger.info('Got card name: %s', card_name)
         if not card_name:
             continue
@@ -159,7 +184,7 @@ def create_jobs_for_cards(filelist):
             condor_file.write(make_job_description(card_name))
 
         os.makedirs(f'{local_dir}/{card_name}/', exist_ok=True)
-        run_command([f'cp -R {dir}/* {local_dir}/{card_name}/',
+        run_command([f'cp -R {directory}/* {local_dir}/{card_name}/',
                      f'chmod +x {local_dir}/{card_name}.sh'])
         logger.info('Submitting %s...', card_name)
         out, err, code = run_command([f'cd {local_dir}',
@@ -168,10 +193,14 @@ def create_jobs_for_cards(filelist):
 
 
 def run():
+    """
+    Main function
+    """
     jobs = get_jobs_in_condor()
     card_names = get_new_cards()
     logger.info('Changed cards: %s', card_names)
     card_names = get_existing_files(card_names)
+    logger.warning('Hardcoded card name!')
     card_names = ['GridpackExtravaganza/Run3_2022/DY_Beep_BoopLO/cards/pdmvtestLO_proc_card.dat']
     logger.info('Existing cards: %s', card_names)
     create_jobs_for_cards(card_names)
