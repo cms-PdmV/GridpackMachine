c70610a45d83e1700d13da4cc4985d2bb25ff9ce
Sihyun Jeon <shjeon@cern.ch>
2023-10-13 14:23:40 -0400

    rename run_card to input_card

--- powheg_gridpack.py
+++ powheg_gridpack.py
@@ -41,17 +41,17 @@ class PowhegGridpack(Gridpack):
 
         return customize_card
 
-    def prepare_run_card(self):
+    def prepare_input_card(self):
         """
-        Get run card and write it to job files dir
+        Get run card and cusomize card, glue them together into input card and write it to job files dir
         """
         job_files_path = self.get_job_files_path()
         output_file_name = os.path.join(job_files_path, 'powheg.input')
-        run_card = self.get_run_card() + '\n\n\n\n' + self.get_customize_card()
-        self.logger.debug('Writing customized run card %s', output_file_name)
-        self.logger.debug(run_card)
+        input_card = self.get_run_card() + '\n\n\n\n' + self.get_customize_card()
+        self.logger.debug('Writing customized input card %s', output_file_name)
+        self.logger.debug(input_card)
         with open(output_file_name, 'w') as output_file:
-            output_file.write(run_card)
+            output_file.write(input_card)
 
     def get_proc_card(self):
         """
@@ -80,7 +80,7 @@ class PowhegGridpack(Gridpack):
         """
         job_files_path = self.get_job_files_path()
         pathlib.Path(job_files_path).mkdir(parents=True, exist_ok=True)
-        self.prepare_run_card()
+        self.prepare_input_card()
         self.prepare_proc_card()
         local_dir = self.local_dir()
         os.system(f'tar -czvf {local_dir}/input_files.tar.gz -C {local_dir} input_files')
